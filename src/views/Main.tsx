f293ccc05aea474b8d00e1d20ec881d9b42f5605
Lingen Liu <lingen.liu@gmail.com>
2022-03-29 10:36:54 +0800

    chore: 升级一些基础依赖

--- src/views/Main.tsx
+++ src/views/Main.tsx
@@ -1,7 +1,5 @@
 import * as React from "react";
 import styles from "style/Main.module.less";
-import Button from "@material-ui/core/Button";
-import GroupIcon from "@material-ui/icons/Group";
 
 const Main = () => {
   return <><div className={styles.root}>https://myddd.org</div></>
